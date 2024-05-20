92911546cf30df1ebc78801fd8ad1e22343bbab6
RoberthGarcia1404 <roberthgarcia219@unisangil.edu.co>
2024-05-19 23:20:06 -0500

    titulo de la pestaña

--- archivos/secciones/header.php
+++ archivos/secciones/header.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Catastro</title>
+  <title>Catastro Chiquinquíra</title>
 
   <link rel="stylesheet" href="../../css/acordeonn.css" />
   <link rel="stylesheet" href="../../css/estilos.css" />
