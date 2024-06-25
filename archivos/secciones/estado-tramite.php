c8088c72f5ec6476f20ed76b38af282bf645e0a3
diazfelipe10 <andres.felipe.1999af@gmail.com>
2024-06-25 14:01:41 -0500

    hola

--- archivos/secciones/estado-tramite.php
+++ archivos/secciones/estado-tramite.php
@@ -10,7 +10,7 @@
     <table>
         <thead>
             <tr>
-            <th>Número de Solicitud</th>
+          
             <th>Número de Radicación</th>
             <th>Fecha de Radicación</th>
             <th>Estado del Trámite</th>
@@ -20,7 +20,7 @@
         </thead>
         <tbody>
             <tr>
-            <td>1</td>
+     
             <td>12345</td>
             <td>2024-05-14</td>
             <td>En proceso</td>
